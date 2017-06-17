bcea1ff477299534a80eb8ce61cf6e29bbf5eb74
c-shekhar <chandrashekhar1503@gmail.com>
2017-06-17 19:29:03 +0530

    added Ridge regression on crime dataset

--- Implementations/adspy_shared_utilities.py
+++ Implementations/adspy_shared_utilities.py
@@ -15,7 +15,7 @@ import matplotlib.patches as mpatches
 def load_crime_dataset():
     # Communities and Crime dataset for regression
     # https://archive.ics.uci.edu/ml/datasets/Communities+and+Crime+Unnormalized
-
+    
     crime = pd.read_table('CommViolPredUnnormalizedData.txt', sep=',', na_values='?')
     # remove features with poor coverage or lower relevance, and keep ViolentCrimesPerPop target column
     columns_to_keep = [5, 6] + list(range(11,26)) + list(range(32, 103)) + [145]  
